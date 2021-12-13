2f05353076dcc0b3b095a6cd5996dd9fca46e195
loquy <841243659@qq.com>
2021-12-13 17:52:30 +0800

    Updated ExcelSheetColumnTitle algorithm

--- src/main/java/com/leetcode/editor/AddTwoNumbers.java
+++ src/main/java/com/leetcode/editor/AddTwoNumbers.java
@@ -40,13 +40,13 @@ public class AddTwoNumbers {
             // 逐个位置相加，大于十补上进位
             int sum = n1 + n2 + carry;
             if (head == null) {
-                // 头节点指向下一个节点引用, 相加结果大于10则下一位加1，此位值为余数，竖式相加
+                // 头节点指向下一个节点引用, 相加结果大于 10 则下一位加 1，此位值为余数，竖式相加
                 head = tail = new ListNode(sum % 10);
             } else {// 循环替换下一个结点，相加结果取余数
                 tail.next = new ListNode(sum % 10);
                 tail = tail.next;
             }
-            // 十进制相加，进位，小于10取0.几的整数
+            // 利用除法取整数的进位数
             carry = sum / 10;
             if (l1 != null) {
                 l1 = l1.next;
@@ -56,7 +56,7 @@ public class AddTwoNumbers {
             }
         }
         if (carry > 0) {
-            // 最后一位相加的大于10，则尾部补上进位值
+            // 最后一位相加的大于 10，则尾部补上进位值
             tail.next = new ListNode(carry);
         }
         // 就此两数相加结果数，只不过顺序相反，因为是从数的尾部开始逐个位置相加
