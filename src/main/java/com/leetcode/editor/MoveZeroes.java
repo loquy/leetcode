d33ed79336103bc221d06ba2fa017561765b443b
loquy <841243659@qq.com>
2021-12-24 14:59:11 +0800

    Updated MissingNumber algorithm,MoveZeroes algorithm, RangeSumQueryImmutable algorithm

---
+++ src/main/java/com/leetcode/editor/MoveZeroes.java
@@ -0,0 +1,54 @@
+package com.leetcode.editor;
+
+import java.util.Arrays;
+
+/**
+ * 移动零
+ *
+ * @author loquy
+ * @date 2021 /12/24 14:08
+ */
+public class MoveZeroes {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        int[] ints = {0, 1, 0, 3, 12};
+        moveZeroes(ints);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    /**
+     * Move zeroes.
+     *
+     * @param nums the nums
+     */
+    public static void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                // 非零元素，左右指针的元素互换，同时左指针右移。
+                // 否则右指针右移。
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    /**
+     * Swap.
+     *
+     * @param nums  the nums
+     * @param left  the left
+     * @param right the right
+     */
+    public static void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
---
+++ src/main/java/com/leetcode/editor/RangeSumQueryImmutable.java
@@ -0,0 +1,55 @@
+package com.leetcode.editor;
+
+/**
+ * 区域和检索 - 数组不可变
+ *
+ * @author loquy
+ * @date 2021 /12/24 14:33
+ */
+public class RangeSumQueryImmutable {
+
+    private static int[] sums;
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        RangeSumQueryImmutable rangeSumQueryImmutable = new RangeSumQueryImmutable(new int[]{-2, 0, 3, -5, 2, -1});
+        int i = sumRange(0, 2);
+        int i1 = sumRange(2, 5);
+        int i2 = sumRange(0, 5);
+        System.out.println(i);
+        System.out.println(i1);
+        System.out.println(i2);
+    }
+
+    /**
+     * Num array.
+     *
+     * @param nums the nums
+     */
+    public RangeSumQueryImmutable(int[] nums) {
+        int n = nums.length;
+        sums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            // sums 的下一个元素等于 nums 当前元素加 sums 当前元素的和
+            // 即 sums 除了第一个元素是 0 ，sums 的下一个元素都是两个数组上一个元素对齐相加的结果
+            // sums: [0, -2, -2, 1, -4, -2, -3]
+            // nums: [-2, 0, 3, -5, 2, -1]
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    /**
+     * Sum range int.
+     *
+     * @param i the
+     * @param j the j
+     * @return the int
+     */
+    public static int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
