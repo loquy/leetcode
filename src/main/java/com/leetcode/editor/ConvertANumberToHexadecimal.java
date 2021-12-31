ff941e624d435cf428921cb5c85dca1303084124
loquy <841243659@qq.com>
2021-12-31 17:50:32 +0800

    Updated SumOfLeftLeaves algorithm,ConvertANumberToHexadecimal algorithm

---
+++ src/main/java/com/leetcode/editor/ConvertANumberToHexadecimal.java
@@ -0,0 +1,47 @@
+package com.leetcode.editor;
+
+/**
+ * 数字转换为十六进制数
+ *
+ * @author loquy
+ * @date 2021 /12/31 9:40
+ */
+public class ConvertANumberToHexadecimal {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        String s = toHex(1);
+        String s1 = toHex(10);
+        String s2 = toHex(100);
+        String s3 = toHex(1000);
+        System.out.println(s);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+
+    /**
+     * To hex string.
+     *
+     * @param num the num
+     * @return the string
+     */
+    public static String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 7; i >= 0; i--) {
+            int val = (num >> (4 * i)) & 0xf;
+            if (sb.length() > 0 || val > 0) {
+                char digit = val < 10 ? (char) ('0' + val) : (char) ('a' + val - 10);
+                sb.append(digit);
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/leetcode/editor/SumOfLeftLeaves.java
@@ -0,0 +1,71 @@
+package com.leetcode.editor;
+
+import com.leetcode.utils.TreeNode;
+
+import java.util.List;
+
+/**
+ * 左叶子之和
+ *
+ * @author loquy
+ * @date 2021 /12/30 14:46
+ */
+public class SumOfLeftLeaves {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        TreeNode left = new TreeNode(4);
+        TreeNode right = new TreeNode(5);
+        TreeNode treeNode = new TreeNode(3, left, right);
+        TreeNode treeNode1 = new TreeNode(6, treeNode, treeNode);
+        TreeNode treeNode2 = new TreeNode(7, treeNode1, treeNode1);
+        TreeNode.show(treeNode2);
+        int i = sumOfLeftLeaves(treeNode2);
+        System.out.println(i);
+    }
+
+    /**
+     * Sum of left leaves int.
+     *
+     * @param root the root
+     * @return the int
+     */
+    public static int sumOfLeftLeaves(TreeNode root) {
+        return root != null ? dfs(root) : 0;
+    }
+
+    /**
+     * Dfs int.
+     *
+     * @param node the node
+     * @return the int
+     */
+    public static int dfs(TreeNode node) {
+        // 深度优先搜索，递归遍历左右子树
+        int ans = 0;
+        if (node.left != null) {
+            // 左子树不等于null，是叶子节点则返回节点值，否则递归遍历左子树
+            ans += isLeafNode(node.left) ? node.left.val : dfs(node.left);
+        }
+
+        if (node.right != null && !isLeafNode(node.right)) {
+            // 右子树不等于null，且不为叶子节点，则递归遍历右子树
+            ans += dfs(node.right);
+        }
+        return ans;
+    }
+
+    /**
+     * Is leaf node boolean.
+     *
+     * @param node the node
+     * @return the boolean
+     */
+    public static boolean isLeafNode(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+}
